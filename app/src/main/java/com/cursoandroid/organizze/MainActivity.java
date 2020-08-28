30dc50449df63504a6286936dbbbfdb863ee9e46
Natanael <natanael@timipro.com.br>
2020-08-28 04:59:05 -0300

    Primeira versão do app

---
+++ app/src/main/java/com/cursoandroid/organizze/MainActivity.java
@@ -0,0 +1,58 @@
+package com.cursoandroid.organizze;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.cursoandroid.organizze.activity.CadastroActivity;
+import com.cursoandroid.organizze.activity.LoginActivity;
+import com.heinrichreimersoftware.materialintro.app.IntroActivity;
+import com.heinrichreimersoftware.materialintro.app.SlideFragment;
+import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
+
+public class MainActivity extends IntroActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setButtonBackVisible(false);
+        setButtonNextVisible(false);
+
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_1)
+                .build());
+
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_2)
+                .build());
+
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_3)
+                .build());
+
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_4)
+                .build());
+
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_cadastro)
+                .build());
+    }
+
+    public void btnEntrar(View view) {
+        startActivity(new Intent(this, LoginActivity.class));
+    }
+
+    public void btnCadastrar(View view)
+    {
+        startActivity(new Intent(this, CadastroActivity.class));
+    }
+}
