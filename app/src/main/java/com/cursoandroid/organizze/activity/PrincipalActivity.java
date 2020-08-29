0b472e8cd174a7582c4bfed8b8306dc609e0141b
Natanael <natanael@timipro.com.br>
2020-08-29 20:35:42 -0300

    Criado interface Principal

--- app/src/main/java/com/cursoandroid/organizze/activity/PrincipalActivity.java
+++ app/src/main/java/com/cursoandroid/organizze/activity/PrincipalActivity.java
@@ -5,16 +5,32 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
+
 import com.cursoandroid.organizze.R;
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnMonthChangedListener;
 
 public class PrincipalActivity extends AppCompatActivity {
+    private TextView textoSaudacao, textoSaldo;
 
+    private MaterialCalendarView calendarView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        calendarView = findViewById(R.id.calendarView);
+        configurarCalendarView();
+
+        textoSaudacao = findViewById(R.id.textSaudacao);
+        textoSaldo = findViewById(R.id.textSaldo);
+
+
+
     }
 
     public void adicionarReceita(View view) {
@@ -24,4 +40,17 @@ public class PrincipalActivity extends AppCompatActivity {
     public void adicionarDespesa(View view) {
         startActivity(new Intent(this, DespesasActivity.class));
     }
+
+    public  void configurarCalendarView() {
+
+        calendarView.setOnMonthChangedListener(new OnMonthChangedListener() {
+            @Override
+            public void onMonthChanged(MaterialCalendarView widget, CalendarDay date) {
+
+            }
+        });
+
+    }
+
+
 }
