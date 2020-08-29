701888dc0a9e1fb5d51716f33aa92f1cbda1a7c5
Natanael <natanael@timipro.com.br>
2020-08-29 01:25:41 -0300

    Adicionado Floating Action Menu

--- app/src/main/java/com/cursoandroid/organizze/activity/PrincipalActivity.java
+++ app/src/main/java/com/cursoandroid/organizze/activity/PrincipalActivity.java
@@ -20,15 +20,14 @@ public class PrincipalActivity extends AppCompatActivity {
         setContentView(R.layout.activity_principal);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+    }
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+    public void adicionarReceita(View view){
+
+    }
+
+    public void adicionarDespesa(View view){
+
     }
 
 }
