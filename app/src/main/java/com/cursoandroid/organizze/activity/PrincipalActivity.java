ff84790f520a1fc75a406a95e3748f71818efb3a
Natanael <natanael@timipro.com.br>
2020-08-29 22:23:42 -0300

    Listar Movimentações

--- app/src/main/java/com/cursoandroid/organizze/activity/PrincipalActivity.java
+++ app/src/main/java/com/cursoandroid/organizze/activity/PrincipalActivity.java
@@ -6,16 +6,22 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.CpuUsageInfo;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.cursoandroid.organizze.R;
+import com.cursoandroid.organizze.adapter.AdapterMovimentacao;
 import com.cursoandroid.organizze.config.ConfiguracaoFirebase;
 import com.cursoandroid.organizze.helper.Base64Custom;
+import com.cursoandroid.organizze.model.Movimentacao;
 import com.cursoandroid.organizze.model.Usuario;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -27,6 +33,8 @@ import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 import com.prolificinteractive.materialcalendarview.OnMonthChangedListener;
 
 import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PrincipalActivity extends AppCompatActivity {
     private TextView textoSaudacao, textoSaldo;
@@ -42,6 +50,16 @@ public class PrincipalActivity extends AppCompatActivity {
     private Double receitaTotal = 0.00;
     private Double resumo = 0.00;
 
+    private RecyclerView recyclerView;
+
+    private AdapterMovimentacao adapterMovimentacao;
+    private List<Movimentacao> movimentacoes = new ArrayList<>();
+
+    private DatabaseReference movimentacaoRef;
+
+    private String mesAnoSelecionado;
+
+    private ValueEventListener valueEventListenerMovimentacoes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,6 +74,106 @@ public class PrincipalActivity extends AppCompatActivity {
 
         textoSaudacao = findViewById(R.id.textSaudacao);
         textoSaldo = findViewById(R.id.textSaldo);
+
+        recyclerView = findViewById(R.id.recyclerMovimentos);
+
+        //configurar adapter
+        adapterMovimentacao = new AdapterMovimentacao(movimentacoes, this);
+
+
+        //Configurar um RecyclerView
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setAdapter(adapterMovimentacao);
+    }
+
+    public void adicionarReceita(View view) {
+        startActivity(new Intent(this, ReceitasActivity.class));
+    }
+
+    public void adicionarDespesa(View view) {
+        startActivity(new Intent(this, DespesasActivity.class));
+    }
+
+    public void configurarCalendarView() {
+
+        CalendarDay dataAtual = calendarView.getCurrentDate();
+
+        String mesSelecionado = String.format("%02d", (dataAtual.getMonth()));
+
+        mesAnoSelecionado = String.valueOf(mesSelecionado + "" + dataAtual.getYear());
+
+        calendarView.setOnMonthChangedListener(new OnMonthChangedListener() {
+            @Override
+            public void onMonthChanged(MaterialCalendarView widget, CalendarDay date) {
+                String mesSelecionado = String.format("%02d", (date.getMonth() ));
+                mesAnoSelecionado = String.valueOf(mesSelecionado + "" + date.getYear());
+
+                movimentacaoRef.removeEventListener(valueEventListenerMovimentacoes);
+                recuperarMovimentacao();
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_principal, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_sair:
+                autenticacao.signOut();
+                startActivity(new Intent(this, MainActivity.class));
+                finish();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void recuperarMovimentacao () {
+        String emailUsuario = autenticacao.getCurrentUser().getEmail();
+        String idUsuario = Base64Custom.codificarBase64(emailUsuario);
+
+
+        movimentacaoRef = firebaseRef.child("movimentacao")
+                .child(idUsuario)
+                .child(mesAnoSelecionado);
+        valueEventListenerMovimentacoes = movimentacaoRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                movimentacoes.clear();
+
+
+                for (DataSnapshot dados: dataSnapshot.getChildren()){
+                    Movimentacao movimentacao = dados.getValue(Movimentacao.class);
+
+                    movimentacoes.add(movimentacao);
+                }
+
+                adapterMovimentacao.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        recuperarResumo();
+        recuperarMovimentacao();
     }
 
     private void recuperarResumo() {
@@ -89,53 +207,10 @@ public class PrincipalActivity extends AppCompatActivity {
 
     }
 
-    public void adicionarReceita(View view) {
-        startActivity(new Intent(this, ReceitasActivity.class));
-    }
-
-    public void adicionarDespesa(View view) {
-        startActivity(new Intent(this, DespesasActivity.class));
-    }
-
-    public void configurarCalendarView() {
-
-        calendarView.setOnMonthChangedListener(new OnMonthChangedListener() {
-            @Override
-            public void onMonthChanged(MaterialCalendarView widget, CalendarDay date) {
-
-            }
-        });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_principal, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.menu_sair:
-                autenticacao.signOut();
-                startActivity(new Intent(this, MainActivity.class));
-                finish();
-                break;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     @Override
     protected void onStop() {
         super.onStop();
         usuarioRef.removeEventListener(valueEventListenerUsuario);
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        recuperarResumo();
+        movimentacaoRef.removeEventListener(valueEventListenerMovimentacoes);
     }
 }
---
+++ app/src/main/java/com/cursoandroid/organizze/adapter/AdapterMovimentacao.java
@@ -0,0 +1,70 @@
+package com.cursoandroid.organizze.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.cursoandroid.organizze.R;
+import com.cursoandroid.organizze.model.Movimentacao;
+
+import java.util.List;
+
+public class AdapterMovimentacao extends RecyclerView.Adapter<AdapterMovimentacao.MyViewHolder>{
+
+    List<Movimentacao> movimentacoes;
+    Context context;
+
+    public AdapterMovimentacao(List<Movimentacao> movimentacoes, Context context) {
+        this.movimentacoes = movimentacoes;
+        this.context = context;
+    }
+
+    @Override
+    public AdapterMovimentacao.MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemLista = LayoutInflater.from(parent.getContext()).inflate(R.layout.adapter_movimentacao, parent, false);
+        return new AdapterMovimentacao.MyViewHolder(itemLista);
+    }
+
+
+    @Override
+    public void onBindViewHolder(AdapterMovimentacao.MyViewHolder holder, int position) {
+        Movimentacao movimentacao = movimentacoes.get(position);
+
+        holder.titulo.setText(movimentacao.getDescricao());
+        holder.valor.setText(String.valueOf(movimentacao.getValor()));
+        holder.categoria.setText(movimentacao.getCategoria());
+        holder.valor.setTextColor(context.getResources().getColor(R.color.colorAccentReceita));
+
+        if (movimentacao.getTipo() == "d" || movimentacao.getTipo().equals("d")) {
+            holder.valor.setTextColor(context.getResources().getColor(R.color.colorAccent));
+            holder.valor.setText("-" + movimentacao.getValor());
+        }
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return movimentacoes.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+
+        TextView titulo, valor, categoria;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+
+            titulo = itemView.findViewById(R.id.textAdapterTitulo);
+            valor = itemView.findViewById(R.id.textAdapterValor);
+            categoria = itemView.findViewById(R.id.textAdapterCategoria);
+        }
+
+    }
+
+}
+
+
